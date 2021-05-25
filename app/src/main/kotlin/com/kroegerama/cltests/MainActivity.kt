3aa6d6a9dc18000d39bbdcbadb0aa4a8a5208ac8
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-05-25 21:30:18 +0200

    add spring bug

--- app/src/main/kotlin/com/kroegerama/cltests/MainActivity.kt
+++ app/src/main/kotlin/com/kroegerama/cltests/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kroegerama.cltests
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
@@ -10,15 +11,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<MaterialButton>(R.id.btnWrapContent).setOnClickListener {
-            start<AcWrapContentBug>()
-        }
-        findViewById<MaterialButton>(R.id.btnSwipe).setOnClickListener {
-            start<AcSwipeBug>()
-        }
+        findViewById<MaterialButton>(R.id.btnWrapContent).startOnClick<AcWrapContentBug>()
+        findViewById<MaterialButton>(R.id.btnSwipe).startOnClick<AcSwipeBug>()
+        findViewById<MaterialButton>(R.id.btnSpring).startOnClick<AcSpringBug>()
     }
 
-    private inline fun <reified T : Activity> start() {
+    private inline fun <reified T : Activity> View.startOnClick() = setOnClickListener {
         startActivity(Intent(this@MainActivity, T::class.java))
     }
 }
@@ -26,3 +24,5 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 class AcWrapContentBug : AppCompatActivity(R.layout.wrap_content_bug)
 
 class AcSwipeBug : AppCompatActivity(R.layout.swipe_bug)
+
+class AcSpringBug : AppCompatActivity(R.layout.spring_bug)
